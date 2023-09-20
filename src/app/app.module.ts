6befe89384513418aee055af6cbc09d5eea8c9c4
Anthony Cruz <ms_anthony_cruz@hotmail.com>
2023-09-19 21:09:12 -0500

    rutas y pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // Routing
 import { APP_ROUTING } from './app.routes';
-// Services
-
 // Component
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+// Services
+import { HeroesService } from './services/heroes.service';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -17,13 +18,14 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [ HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Heroe, HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styles: [
-  ]
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
 
+  constructor(private _heroesService: HeroesService, private router: Router) {}
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(index: number) {
+    this.router.navigate(['/heroe', index]);
+  }
 }
